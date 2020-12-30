e2feec19e3b4f90a6c97c89860f95865b93a65c0
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-12-30 17:02:18 -0500

    Added the Cerulean theme, and set up the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+import React from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <h1>Welcome to BuyMyStuff!</h1>
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to BuyMyStuff!</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
