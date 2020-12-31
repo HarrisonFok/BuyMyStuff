fa38f8a1f3678fefd107925742422344546b0f51
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-12-31 17:14:25 -0500

    Added the quantity select for each product, and also a handler for adding to a cart; Added basic Cart Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,15 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers"
+import { cartReducer } from "./reducers/cartReducers"
 
 // will have multiple reducers handling a certain piece of functionalities (eg. reducer for product)
 
 // the keys will show in the redux store
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 
 const initialState = {}
