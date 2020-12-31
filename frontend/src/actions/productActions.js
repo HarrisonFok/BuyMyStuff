d6afa861f8a3c90f06705eb52adcb7c71d14e2e4
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-12-31 15:36:44 -0500

    Removed axios calls in ProductScreen component and calling the action instead in the useEffect (to fill up the state); Used useSelector to select the productDetails part of the state

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
 import axios from "axios";
 // want to make an async request - redux-thunk allows you to add a function within a function
 
@@ -16,5 +16,20 @@ export const listProducts = () => async(dispatch) => {
         // the message will get us the error message we got when we tried wrong things on postman
         // error.response.data.message is here because we have the custom middleware
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message})
+    }
+} 
+
+export const listProductDetails = (id) => async(dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST});
+        const {data} = await axios.get(`/api/products/${id}`)
+        // in productReducer, when it's success, the payload will be matched to the "products" key
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS, 
+            payload: data
+        })
+    } catch (error) {
+        // the message will get us the error message we got when we tried wrong things on postman
+        dispatch({type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message})
     }
 } 
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,6 @@
 // This will handle the state for the product list on the home page
 
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
 
 export const productListReducer = (state={ products: [] }, action) => {
     // 3 types: request, success, failure
@@ -15,3 +15,17 @@ export const productListReducer = (state={ products: [] }, action) => {
             return state
     }
 } 
+
+export const productDetailsReducer = (state={ product: { reviews: []} }, action) => {
+    // 3 types: request, success, failure
+    switch(action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+           return { loading: true, ...state }
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
