57de1b566b82e88209397e685e8a268889eea997
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-12-31 17:54:48 -0500

    Implemented CartScreen and added remove to cart actions

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 // getState allows you to get your entire state tree (i.e. the combined reducers)
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -19,4 +19,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   // Save in local storage
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+      type: CART_REMOVE_ITEM,
+      payload: id,
+    });
+  
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
