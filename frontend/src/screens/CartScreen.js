be57974cde4287cb293b499f8ed1d88396e9e83f
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-12-31 17:42:47 -0500

    Implemented 'add to cart' functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+// deal with redux states with functional components
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
-    return (
-        <div>
-            Cart
-        </div>
-    )
+const CartScreen = ({match, location, history}) => {
+    const productId = match.params.id;
+
+    // query params - including and after ?
+    const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  
+    const dispatch = useDispatch();
+  
+    // Grab items from redux for the cart items 
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+  
+    // only want to dispatch ADD_TO_CART if we have product id
+    useEffect(() => {
+      if (productId) {
+        dispatch(addToCart(productId, qty));
+      }
+    }, [dispatch, productId, qty]);
+
+    return <div>Cart</div>;
 }
 
 export default CartScreen
