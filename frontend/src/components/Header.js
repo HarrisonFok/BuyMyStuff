f5eb15a802ef4e635d4b747c5a9d0eb7186cb74b
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-01 14:54:30 -0500

    Implemented logout functionality and the header to dynamically show the name if the user is logged in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import {Navbar, Nav, Container} from "react-bootstrap";
+// fire an action - useDispatch; bring something in from redux - useSelector
+import {useDispatch, useSelector} from "react-redux"
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 // wrap bootstrap components
 import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin);
+    const {userInfo} = userLogin;
+
+    const logoutHandler = () => {
+        // console.log("logout")
+        dispatch(logout())
+    }
+    
     return (
         <header>
             <Navbar bg="light" expand="lg" collapseOnSelect>
@@ -17,9 +30,22 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? 
+                                (
+                                    // allow the user to have a dropdown to either profile or logout
+                                    <NavDropdown title={userInfo.name} id="username">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                    </NavDropdown>
+                                ) :
+                                (
+                                    <LinkContainer to="/login">
+                                        <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
+                                    </LinkContainer>
+                                )
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
