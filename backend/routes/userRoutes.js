ce51b7dd56c491431756649af1a5352198766ea8
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-03 13:36:30 -0500

    Implemented admin delete user functionality: added to controller, route; Implemented user delete reducer (added to store.js) and action; Passed successDelete into dependency so that the page will run again after a delete; Dispatched the delete user action in the delete handler

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import {authUser, getUserProfile, registerUser, updateUserProfile, getUsers} from "../controllers/userController.js";
+import {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser} from "../controllers/userController.js";
 import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(protect, isAdmin, getUsers)
 router.post("/login", authUser)
 // middleware as the first argument
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/:id").delete(protect, isAdmin, deleteUser)
 
 export default router;
