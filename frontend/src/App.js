6f797adb3afbeb42da115ab4207823e55f748d7a
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-07 18:50:33 -0500

    Handled non-existing URLs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import NotFoundScreen from "./screens/NotFoundScreen";
 
 const App = () => {
   return (
@@ -47,6 +48,7 @@ const App = () => {
           <Route path="/page/:pageNumber" component={HomeScreen} exact />
           <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} exact />
           <Route path="/" component={HomeScreen} exact/>
+          <Route component={NotFoundScreen}/>
         </Container>
       </main>
       <Footer />
