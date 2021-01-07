732584ff7f23abd63d4251a0369df863cf5bc2d0
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-07 18:57:05 -0500

    Handled the error where the NotFoundScreen component is showing on every page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import {Container} from "react-bootstrap";
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -26,29 +26,31 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/order/:id" component={OrderScreen} />
-          <Route path="/shipping" component={ShippingScreen} />
-          <Route path="/payment" component={PaymentScreen} />
-          <Route path="/placeorder" component={PlaceOrderScreen} />
-          <Route path="/login" component={LoginScreen} />
-          <Route path="/register" component={RegisterScreen} />
-          <Route path="/profile" component={ProfileScreen} />
-          <Route path="/product/:id" component={ProductScreen} />
-          {/* optional */}
-          <Route path="/cart/:id?" component={CartScreen} />
-          <Route path="/admin/userList" component={UserListScreen} />
-          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-          {/* Without the exact for the two below routes, it'll show duplicate screeens */}
-          <Route path="/admin/productList" component={ProductListScreen} exact/>
-          <Route path="/admin/productList/:pageNumber" component={ProductListScreen} exact/>
-          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
-          <Route path="/admin/orderList" component={OrderListScreen} />
-          <Route path="/search/:keyword" component={HomeScreen} />
-          {/* If there's a keyword, then use that to search; otherwise, just use page number  */}
-          <Route path="/page/:pageNumber" component={HomeScreen} exact />
-          <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} exact />
-          <Route path="/" component={HomeScreen} exact/>
-          <Route component={NotFoundScreen}/>
+          <Switch>
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            {/* optional */}
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/admin/userList" component={UserListScreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+            {/* Without the exact for the two below routes, it'll show duplicate screeens */}
+            <Route path="/admin/productList" component={ProductListScreen} exact/>
+            <Route path="/admin/productList/:pageNumber" component={ProductListScreen} exact/>
+            <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+            <Route path="/admin/orderList" component={OrderListScreen} />
+            <Route path="/search/:keyword" component={HomeScreen} />
+            {/* If there's a keyword, then use that to search; otherwise, just use page number  */}
+            <Route path="/page/:pageNumber" component={HomeScreen} exact />
+            <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} exact />
+            <Route path="/" component={HomeScreen} exact/>
+            <Route component={NotFoundScreen}/>
+          </Switch>
         </Container>
       </main>
       <Footer />
