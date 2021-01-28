c672dbf7c96e7f7df87378651c0fecbd08ebf1b4
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-27 21:37:36 -0500

    Revert "Trying to link page to chat app"
    This reverts commit 2efea3b9eb4ab1571db1cf9fcbc9448fa520707c.

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { execChatApp } from "../controllers/chatController.js";
-import { protect } from "../middleware/authMiddleware.js";
-const router = express.Router();
-
-router.route("/letsChat").get(protect, execChatApp)
-
-export default router;
--- frontend/src/constants/chatConstants.js
+++
@@ -1,3 +0,0 @@
-export const GET_CHAT = "GET_CHAT";
-export const GET_CHAT_SUCCESS = "GET_CHAT_SUCCESS";
-export const GET_CHAT_FAIL = "GET_CHAT_FAIL";
