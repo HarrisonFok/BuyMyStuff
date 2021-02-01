411a32c3b399e88cce2d428c85246b88198ca8d9
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-31 20:53:09 -0500

    Chat App is now getting the username and password in clear text (from localstorage); Need to figure out how to automatically register user to react-chat-engine upon button click

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -10,8 +10,8 @@ const App = () => {
         <ChatEngine 
             height="100vh"
             projectID="89a5f4e9-5a8e-47cf-97e9-9270d3c314ff"
-            // userName={localStorage.getItem("username")}
-            // userSecret={localStorage.getItem("password")}
+            userName={localStorage.getItem("username")}
+            userSecret={localStorage.getItem("password")}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
